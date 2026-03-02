67bc541e4770f7e0aa92ac2d863b7ca25463cd64
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-03-02 14:39:41 +0700

    Completed practical work  №4

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@
 let app = new Vue({
    el: '#app',
    data: {
-       product: "Socks",
-       description: "A pair of warm, fuzzy socks",
-       image: "./assets/vmSocks-blue-onWhite.jpg",
-       altText: "A pair of socks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inventory: 5,
-       onSale: true,
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
